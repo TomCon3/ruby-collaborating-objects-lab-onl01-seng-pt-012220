8021689cb5ae88df9aee86884c36d6e9439d7ad0
Tommy Connor <tconnor98@aol.com>
2020-02-17 16:42:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,10 +8,13 @@ class Song
     @name = name
     save
   end
+  
+  def save
+    @all << self
+  end
 
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
-    @@all << song
     artist = file_name.split(" - ")[0]
     new_song = self.new(song)
     new_song.artist_name = artist
