be87e694088ddba418c443d5db3287f0a809f122
Tommy Connor <tconnor98@aol.com>
2020-02-17 16:46:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,7 @@ class Song
 
   def initialize(name)
     @name = name
+    self.all
   end
 
   def self.new_by_filename(file_name)
@@ -16,14 +17,13 @@ class Song
     new_song
   end
   
-  def self.all
-    @all
-  end
-  
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
     @@all << self
   end
-
+  
+  def self.all
+    @all
+  end
 end
